d3a40c4d03ba796dbf72101994ce7f03ecff0f3a
David Carlyn <davidecarlyn@gmail.com>
2025-03-18 23:26:15 -0400

    Moved the client connection and server hosting inputs to own screens

--- src/my_place/ui/screens/home.py
+++ src/my_place/ui/screens/home.py
@@ -2,11 +2,17 @@ from kivy.uix.screenmanager import ScreenManager
 
 from my_place.ui.screens.login import LoginScreen
 from my_place.ui.screens.chat import ChatScreen
+from my_place.ui.screens.client import ClientLoginScreen
+from my_place.ui.screens.server import ServerLoginScreen
 
 class HomeScreen(ScreenManager):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.login_screen = LoginScreen(name="login")
+        self.client_login_screen = ClientLoginScreen(name="client-login")
+        self.server_login_screen = ServerLoginScreen(name="server-login")
         self.chat_screen = ChatScreen(name="chat")
         self.add_widget(self.login_screen)
+        self.add_widget(self.client_login_screen)
+        self.add_widget(self.server_login_screen)
         self.add_widget(self.chat_screen)
--- src/my_place/ui/screens/login.py
+++ src/my_place/ui/screens/login.py
@@ -1,4 +1,4 @@
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.uix.label import Label
@@ -10,110 +10,39 @@ from my_place.core.networking.interface import NetworkInterface
 class LoginScreen(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
-        layout = BoxLayout(
-            orientation="vertical"
+        layout = GridLayout(
+            rows=2,
+            cols=1,
         )
-        
-        ## SERVER PART
-        
-        # Username input
-        server_username_input_layout = BoxLayout(orientation="horizontal")
-        server_username_input_lbl = Label(text="Username:")
-        self.server_username_text_input = TextInput(multiline=False)
-        server_username_input_layout.add_widget(server_username_input_lbl)
-        server_username_input_layout.add_widget(self.server_username_text_input)
-        
-        # Port input
-        server_port_input_layout = BoxLayout(orientation="horizontal")
-        server_port_input_lbl = Label(text="Port:")
-        self.server_port_text_input = TextInput(text="40674", multiline=False)
-        server_port_input_layout.add_widget(server_port_input_lbl)
-        server_port_input_layout.add_widget(self.server_port_text_input)
         
         # Host Server btn
         self.host_server_btn = Button(
             background_color=(54/255, 109/255, 59/255, 1),
             background_normal="",
-            size_hint=(.3, .3),
+            size_hint=(.8, .5),
             text = "Host Server",
             font_size=24,
         )
-        self.host_server_btn.bind(on_press=self.host_server)
-        
-        layout.add_widget(server_username_input_layout)
-        layout.add_widget(server_port_input_layout)
+        self.host_server_btn.bind(on_press=self.go_to_server_login_screen)
         layout.add_widget(self.host_server_btn)
-        
-        
-        ## CLIENT PART
-        
-        # Username
-        # Username input
-        client_username_input_layout = BoxLayout(orientation="horizontal")
-        client_username_input_lbl = Label(text="Username:")
-        self.client_username_text_input = TextInput(multiline=False)
-        client_username_input_layout.add_widget(client_username_input_lbl)
-        client_username_input_layout.add_widget(self.client_username_text_input)
-        layout.add_widget(client_username_input_layout)
-        
-        # Address input
-        client_address_input_layout = BoxLayout(orientation="horizontal")
-        client_address_input_lbl = Label(text="Network Address (IP):")
-        self.client_address_text_input = TextInput(multiline=False)
-        client_address_input_layout.add_widget(client_address_input_lbl)
-        client_address_input_layout.add_widget(self.client_address_text_input)
-        layout.add_widget(client_address_input_layout)
-        
-        # Port input
-        client_port_input_layout = BoxLayout(orientation="horizontal")
-        client_port_input_lbl = Label(text="Port:")
-        self.client_port_text_input = TextInput(text="40674", multiline=False)
-        client_port_input_layout.add_widget(client_port_input_lbl)
-        client_port_input_layout.add_widget(self.client_port_text_input)
-        layout.add_widget(client_port_input_layout)
         
         # Connect to Server
         self.connect_to_server_btn = Button(
-            background_color=(54/255, 109/255, 59/255, 1),
+            background_color=(54/255, 109/255, 150/255, 1),
             background_normal="",
-            size_hint=(.3, .3),
+            size_hint=(.8, .5),
             text = "Connect to Server",
             font_size=24,
         )
-        self.connect_to_server_btn.bind(on_press=self.connect_to_server)
+        self.connect_to_server_btn.bind(on_press=self.go_to_client_login_screen)
         layout.add_widget(self.connect_to_server_btn)
         
         self.add_widget(layout)
         
-    def host_server(self, instance):
-        try:
-            port = int(self.server_port_text_input.text)
-            username = self.server_username_text_input.text
-            username = "UNKNOWN" if username == "" else username
-        except Exception as e:
-            print(e)
-            return
-        
-        network : NetworkInterface = App.get_running_app().network
-        network.setup_server(username=username, port=port, num_connections=10)
-        self.go_to_chat_screen()
-    
-    def connect_to_server(self, instance):
-        try:
-            port = int(self.client_port_text_input.text)
-            address = self.client_address_text_input.text
-            username = self.client_username_text_input.text
-            username = "UNKNOWN" if username == "" else username
-        except Exception as e:
-            print(e)
-            return
-        
-        network : NetworkInterface = App.get_running_app().network
-        network.setup_client(username=username, address=address, port=port)
-        self.go_to_chat_screen()
-        
-    def go_to_chat_screen(self):
+    def go_to_server_login_screen(self, instance):
         app = App.get_running_app()
-        app.network.subscribe_to_messages(app.screen_manager.chat_screen.add_chat_item)
+        app.root.current = "server-login"
         
-        app.root.current = "chat"
+    def go_to_client_login_screen(self, instance):
+        app = App.get_running_app()
+        app.root.current = "client-login"
